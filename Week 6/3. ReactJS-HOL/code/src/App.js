11534af0a5cac77dac87d3764ed02d274f02e3bb
Janhvi067 <142768099+Janhvi067@users.noreply.github.com>
2025-07-24 02:29:07 +0530

    Adding hands on-1,2,3

---
+++ Week 6/3. ReactJS-HOL/code/src/App.js
@@ -0,0 +1,15 @@
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name="Steeve" 
+      School="XYZ School" 
+      total={284} 
+      goal={3} />
+
+    </div>
+  );
+}
+
+export default App;
